05fce5d3681108b4ef302e34234bdc2ddbcc612f
Dezaya <obidesire11@gmail.com>
2023-01-24 03:24:09 -0800

    Team Task

--- test_files/_printf.c
+++ test_files/_printf.c
@@ -2,7 +2,8 @@
 #include "main.h"
 #include <stdio.h>
 
-int _printf(const char *format, ...){
+int _printf(const char *format, ...)
+{
   int count = 0, i;
   
   va_list data;
@@ -10,7 +11,8 @@ int _printf(const char *format, ...){
 
   /* _printf("%s", 'Hello') */
   
-  for (i = 0; format[i] != '\0'; ){
+  for (i = 0; format[i] != '\0'; )
+  {
     
     /* count the number of characters */
     /* print to the screen each character counted */
